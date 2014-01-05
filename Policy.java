cbaaf78ac75fcee2bfb463368681d07e31e58baa
codecrackers123 <codecrackers123@gmail.com>
2014-01-06 01:11:10 +0530

    Create Policy.java

---
+++ Policy.java
@@ -0,0 +1,57 @@
+package insurancebl;
+
+public class Policy {
+
+	private String policyId;
+	private String policyname;
+	private double coverageAmount;
+
+	public Policy() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	private double premiumAmount;
+	private String ageGroup;
+
+	public String getPolicyId() {
+		return policyId;
+	}
+
+	public void setPolicyId(String policyId) {
+		this.policyId = policyId;
+	}
+
+	public String getPolicyname() {
+		return policyname;
+	}
+
+	public void setPolicyname(String policyname) {
+		this.policyname = policyname;
+	}
+
+	public double getCoverageAmount() {
+		return coverageAmount;
+	}
+
+	public void setCoverageAmount(double coverageAmount) {
+		this.coverageAmount = coverageAmount;
+	}
+
+	public double getPremiumAmount() {
+		return premiumAmount;
+	}
+
+	public void setPremiumAmount(double premiumAmount) {
+		this.premiumAmount = premiumAmount;
+	}
+
+	public String getAgeGroup() {
+		return ageGroup;
+	}
+
+	public void setAgeGroup(String ageGroup) {
+		this.ageGroup = ageGroup;
+	}
+
+}
